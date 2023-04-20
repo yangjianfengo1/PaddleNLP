696108cc377ac2a98b19f1d483d319995e3bf479
yangjianfengo1 <yangjianfeng01@baidu.com>
2023-04-20 11:18:40 +0000

    text_encode finish

--- ppdiffusers/deploy/apply_loara_paddle_dynamic.py
+++ ppdiffusers/deploy/apply_loara_paddle_dynamic.py
@@ -1,10 +1,7 @@
 from safetensors import safe_open
-from safetensors.numpy import save_file
-from safetensors.numpy import load as numpy_load
 import numpy as np
 import paddle
 import copy
-import os
 from ppdiffusers import (
     FastDeployRuntimeModel,
     FastDeployStableDiffusionInpaintPipeline,
@@ -17,9 +14,6 @@ from ppdiffusers import (
 lora_file_dir = "/home/yangjianfeng01/apply_lora_diffusers/loras/"
 lora_file_list  = ["mecha.safetensors", "Chibi.safetensors", "Colorwater.safetensors", "MagazineCover.safetensors"]
 model_path = "./runwayml/stable-diffusion-v1-5"
-layers_num = 12
-torch_layer_name = ["mlp_fc1", "mlp_fc2", "self_attn_q_proj", "self_attn_k_proj", "self_attn_v_proj", "self_attn_out_proj"]
-
 
 def load_lora_file_weight(lora_file_dir, lora_file_list):
     lora_state_dict_list = []
@@ -32,31 +26,26 @@ def load_lora_file_weight(lora_file_dir, lora_file_list):
     return lora_state_dict_list
 
 def get_text_encode_lora_weight(lora_state_dict_list, dtype=paddle.float32):
-    text_encode_lora_weight = []
+    text_encode_lora_weight = {}
+    layers_num = 12
     layer_name_layer_name = "lora_te_text_model_encoder_layers_"
+    torch_layer_name = ["mlp_fc1", "mlp_fc2", "self_attn_q_proj", "self_attn_k_proj", "self_attn_v_proj", "self_attn_out_proj"]
+    paddle_layer_name = ["linear1", "linear2", "self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj", "self_attn.out_proj"]
+
     for i in range(layers_num):
-        index = 0
-        ops_lora_weight = []
-        for torch_name in torch_layer_name:
-            lora_weight_list = []
+        for torch_name, paddle_name in zip(torch_layer_name, paddle_layer_name):
+            layer_weight = []
             for static_dict in lora_state_dict_list:
                 up_name = layer_name_layer_name + str(i) + "_" + torch_name + ".lora_up.weight"
                 down_name = layer_name_layer_name + str(i) + "_" + torch_name + ".lora_down.weight"
                 weight_up = paddle.to_tensor(static_dict[up_name]).astype(dtype)
                 weight_down = paddle.to_tensor(static_dict[down_name]).astype(dtype)
                 new_weight = paddle.mm(weight_up, weight_down)
-                lora_weight_list.append(new_weight.T)
-            ops_lora_weight.append(lora_weight_list)
-            
-        text_encode_lora_weight.append(ops_lora_weight)    
+                layer_weight.append(new_weight)
+            name = "layers." + str(i) + "." + paddle_name
+            text_encode_lora_weight[name] = layer_weight
     return text_encode_lora_weight
 
-def get_text_encode_weight_bias():
-    file_path = "./text_encode_weight_bias.safetensors"
-    with open(file_path, "rb") as f:
-        data = f.read()
-    text_encode_weight_bias = numpy_load(data)
-    return text_encode_weight_bias
 
 def get_unet_lora_weight(lora_state_dict_list, dtype=paddle.float32):
     unet_lora_weight = {}
@@ -106,54 +95,30 @@ def get_pipeline(model_path):
     )
     return pipeline
 
-def add_lora_weight(text_encode_lora_weight, lora_idx_list, lora_alpha_list):
-    text_encode_add_lora_weight = []
-    length = len(lora_idx_list)
-    for layer in text_encode_lora_weight:
-        layer_weight = []
-        for sub_op in layer:
-            lora_weight = lora_alpha_list[length - 1] * sub_op[lora_idx_list[length - 1]]
-            for i in range(length - 1):
-                lora_weight += lora_alpha_list[i] * sub_op[lora_idx_list[i]]
-            layer_weight.append(lora_weight)
-        text_encode_add_lora_weight.append(layer_weight)
-    return text_encode_add_lora_weight
-
-def inference(pipeline, text_encode_add_lora_weight):
+def run():
+    lora_state_dict_list = load_lora_file_weight(lora_file_dir, lora_file_list)
+    text_encode_lora_weight = get_text_encode_lora_weight(lora_state_dict_list)
+    unet_lora_weight = get_unet_lora_weight(lora_state_dict_list)
+    pipeline = get_pipeline(model_path)
+    pipeline.text_encoder.load_lora_weight(text_encode_lora_weight)
+    pipeline.unet.load_lora_weight(unet_lora_weight)
     image = pipeline(
             prompt="masterpiece, best quality, 1 girl, mecha", 
-            lora_weight = text_encode_add_lora_weight,
+            lora_idx_list=[0],
+            lora_alpha_list=[0.5],
             negative_prompt="worst quality, low quality, nsfw", 
             guidance_scale=5.0, 
             num_inference_steps=20).images[0]
     image.save(f"res0.jpg")
+    image = pipeline(
+            prompt="masterpiece, best quality, 1 girl, mecha, magazine cover", 
+            lora_idx_list=[0, 3],
+            lora_alpha_list=[0.5, 0.325],
+            negative_prompt="worst quality, low quality, nsfw", 
+            guidance_scale=5.0, 
+            num_inference_steps=20).images[0]
+    image.save(f"res1.jpg")
 
-def save_mode(pipeline, path="./"):
-    text_encoder = paddle.jit.to_static(
-        pipeline.text_encoder,
-        input_spec=[
-            paddle.static.InputSpec(shape=[None, None], dtype="int64", name="input_ids"),# input_ids
-            paddle.static.InputSpec(
-                shape=[layers_num, len(torch_layer_name), None, None], 
-                dtype="float32", name="lora_weight"), #lora_weight
-        ],  
-    )
-    save_path = os.path.join(path, "text_encoder", "inference")
-    paddle.jit.save(text_encoder, save_path)
-
-
-def run():
-    lora_state_dict_list = load_lora_file_weight(lora_file_dir, lora_file_list)
-    text_encode_lora_weight = get_text_encode_lora_weight(lora_state_dict_list)
-    text_encode_weight_bias = get_text_encode_weight_bias()
-    pipeline = get_pipeline(model_path)
-    pipeline.text_encoder.load_weight_bias(text_encode_weight_bias)
-
-    lora_idx_list = [0, 3]
-    lora_alpha_list = [0.5, 0.375]
-    text_encode_add_lora_weight = add_lora_weight(text_encode_lora_weight, lora_idx_list, lora_alpha_list)
-    inference(pipeline, text_encode_add_lora_weight)
-    save_mode(pipeline)
 
 if __name__ == "__main__":
     paddle.set_device(f"gpu:1")
